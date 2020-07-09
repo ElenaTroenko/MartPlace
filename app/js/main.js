f7ec20dbb733b203b46930ab20ba60856d62dcd7
Elena Troenko <troenko1@yandex.ru>
2020-07-09 11:54:19 +0300

    Created first part

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,14 @@
 $(function(){
 
+  $('.product-slider__images').slick({
+    variableWidth: true,
+    slidesToShow: 1,
+    
+  });
 
-
+  $('.release-products__menu-btn').on('click', function () {
+    $('.release-products__menu-list').slideToggle();
+  });
+  
 });
+
